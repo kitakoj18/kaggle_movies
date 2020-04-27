e2bc3a6a92608260095b0714ddfa25a966f10dfb
kitakoj18 <kitakoj18@gmail.com>
2020-04-27 15:48:34 -0700

    refactored create_dataset and created function to remove redundant code from main function and calculate simlarities in this one function

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -11,11 +11,13 @@ import pandas as pd
 
 from scipy.spatial.distance import cosine 
 
-from joins import *
+from movie_info import *
+from user_prefs import *
 
-def calc_sim(user_pref, movie_vec, movie_in_pref=True, normalize=True):
+def calc_sim(user_pref, movie_vec, movie_in_pref, normalize=True):
     '''
-    Calculates similarity of user's genre preferences to genre of movie making prediction on
+    Calculates similarity metric between user's preferences and those same characteristics 
+    of the movie making prediction on
     
     user_pref(np array or sparse array): array of user's (genre/cast/crew) preferences
     movie_vec(np array or sparse array): array of the (genre/cast/crew) of the movie that is being compared
@@ -25,6 +27,8 @@ def calc_sim(user_pref, movie_vec, movie_in_pref=True, normalize=True):
     normalize(boolean): min-max normalize user_pref array if True
     '''
     
+    # if movie is included in user's aggregate vector, need to remove movie vector
+    # before calcuating simlarity metric 
     if movie_in_pref:
         user_pref = user_pref - movie_vec
         
@@ -33,65 +37,68 @@ def calc_sim(user_pref, movie_vec, movie_in_pref=True, normalize=True):
         user_pref = np.true_divide(user_pref - user_pref.min(), user_pref.max() - user_pref.min())
     
     return cosine(user_pref, movie_vec)
-        
 
-# df_movies_watched should be named to data and this DataFrame should only contain 
-# user_id, movie_id, and whether user watched the movie
-def add_genre_sim(df_movies_watched, df_genre_pref, df_movies, df_cred_pref, df_creds):
+
+def get_sim(user_id, movie_id, type_cols, df_user_pref, df_movie_info, movie_in_pref):
+
+    '''
+    Selects user's genre, cast, or crew vector (based off type_cols) from df_user_pref
+    and the movie's genre, cast, or crew vector from df_movie_info and calculates
+    the simlarity between the two vectors
+    '''    
+
+    user_pref = df_user_pref[df_user_pref['user_id'] == user_id]
+    user_pref_vec = user_pref[type_cols].to_numpy()
+
+    movie_info = df_movie_info[df_movie_info['movie_id'] == movie_id]
+    movie_info_vec = movie_info[type_cols].to_numpy()     
+    
+    similarity = calc_sim(user_pref_vec, movie_info_vec, movie_in_pref)
+    
+    return similarity
+
+
+def add_sim(df_user_watched, df_genre_pref, df_cred_pref, df_movie_info):
     '''
     Adds to df_movies_watched the similarity metric of user's genre preferences 
     to genre of movie making prediction on 
     '''
-    data = df_movies_watched.copy()
-    genre_sims = []
-    genres = ['unknown', 'action', 'adventure', 'animation', 'childrens',
+    data = df_user_watched.copy()
+    
+    genre_cols = ['unknown', 'action', 'adventure', 'animation', 'childrens',
                   'comedy', 'crime', 'documentary', 'drama', 'fantasy', 'film-noir', 
                   'horror', 'musical', 'mystery', 'romance', 'sci-fi', 'thriller',
                   'war', 'western']
+    genre_sims = []
     
+    cast_col = ['cast_count']
     cast_sims = []
+    
+    crew_col = ['crew_count']
     crew_sims = []
     
-    for idx, row in df_movies_watched.iterrows():
+    for idx, row in data.iterrows():
         user_id = row['user_id']
         movie_id = row['movie_id']
-        #watched_movie = row['watched_movie']
+        watched_movie = row['watched_movie']
         
-        # get user's genre preference from df_genre_pref
-        user_genre_pref = df_genre_pref[df_genre_pref['user_id'] == user_id]
-        # genre vector
-        genre_pref = user_genre_pref[genres].to_numpy()
-        # get movie's genre from df_movies
-        movie_genre = df_movies[df_movies['movie_id'] == movie_id]
-        # movie's genre vector
-        movie_genre_vec = movie_genre[genres].to_numpy()
-        # here add condition: if watched_movie = 1 then movie_in_pref = True
-        # else if watched_movie = 0 then movie_in_pref = False
-        genre_sim = calc_sim(genre_pref, movie_genre_vec)
-        genre_sims.append(genre_sim)
+        # if user watched movie, need to remove movie vector from user's vector done in calc_sim
+        if watched_movie == 1:
+            movie_in_pref = True
+        else:
+            movie_in_pref = False
+            
+        # get similarity between user's genre preferences to movie genre
+        user_genre_sim = get_sim(user_id, movie_id, genre_cols, df_genre_pref, df_movie_info, movie_in_pref)
+        genre_sims.append(user_genre_sim)
         
-        # get user's cast preference from df_cred_pref
-        user_cast_pref = df_cred_pref[df_cred_pref['user_id'] == user_id]
-        # cast vector
-        cast_pref = user_cast_pref[['cast_count']].to_numpy()
-        # get movie's cast from df_creds
-        movie_cast = df_creds[df_movies['movie_id'] == movie_id]
-        movie_cast_vec = movie_cast[['cast_count']]
-        # get similarity of user cast preferences to movie's cast
-        cast_sim = calc_sim(cast_pref, movie_cast_vec)
-        cast_sims.append(cast_sim)
+        # get similarity between user's cast preferences to movie's cast
+        user_cast_sim = get_sim(user_id, movie_id, cast_col, df_cred_pref, df_movie_info, movie_in_pref)
+        cast_sims.append(user_cast_sim)
         
-        # get user's crew preference from df_cred_pref
-        user_crew_pref = df_cred_pref[df_cred_pref['user_id'] == user_id]
-        # crew vector
-        crew_pref = user_crew_pref[['crew_count']].to_numpy()
-        # get movie's cast from df_creds
-        movie_crew = df_creds[df_movies['movie_id'] == movie_id]
-        movie_crew_vec = movie_crew[['crew_count']]
-        # get similarity of user crew preferences to movie's crew
-        crew_sim = calc_sim(crew_pref, movie_crew_vec)
-        crew_sims.append(crew_sim)
-        
+        # get similarity between user's crew preferences to movie's crew
+        user_crew_sim = get_sim(user_id, movie_id, crew_col, df_cred_pref, df_movie_info, movie_in_pref)
+        crew_sims.append(user_crew_sim)
         
     data['genre_sims'] = genre_sims
     data['cast_sims'] = cast_sims
@@ -102,4 +109,6 @@ def add_genre_sim(df_movies_watched, df_genre_pref, df_movies, df_cred_pref, df_
 if __name__ == '__main__':
     
     df_movie_info = get_movie_info()
+    df_user_genre_pref, df_user_cred_pref, df_user_watched = get_user_tables(df_movie_info)
     
+    data = add_sim(df_user_watched, df_user_genre_pref, df_user_cred_pref, df_movie_info)
